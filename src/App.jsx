7e100f15c66294ddee81ca85f27b2eeb4bd87f04
shibaram-brilliantlink <shibaram@brilliantlink.tech>
2024-04-24 12:30:05 +0530

    01commit-React boiler plate cleanup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+    <h1>Heading 1</h1>
+    <h2>Good Morning </h2>
+    <h2>Good Afternoon</h2>
+    <h2>Good Evening</h2>
+    <h2>Good Night</h2>
+     </div>
+    </>
+  )
+}
+
+export default App
