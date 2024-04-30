4297bc87ffda15780890ba189dc6c3f58e1ad541
shibaram-brilliantlink <shibaram@brilliantlink.tech>
2024-04-30 13:53:29 +0530

    05commit Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,120 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import { TfiDownload } from "react-icons/tfi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [extendedNav, setExtendedNav] = useState(false);
   return (
-    <S_Navbar>
-
-      <Left>
-        <div>
-          <Link to="/">SHIKASHI</Link>
-        </div>
-        <div>
-          Download CV <TfiDownload />
-        </div>
-        <Menu>
-          <li>
-            <Link to="/work">Work</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <GiHamburgerMenu />
-        </Menu>
-      </Left>
-
-      <Menu>
+    <StyledNavbar>
+      <Navmain>
+        <Navleft>
+          <div>
+            <Navlink to="/">SHIKASHI</Navlink>
+          </div>
+          <Navdownload>
+            Download CV <TfiDownload />
+          </Navdownload>
+        </Navleft>
+        <Navright>
+          <Navmenu>
+            <li>
+              <Navlink to="/work">Work</Navlink>
+            </li>
+            <li>
+              <Navlink to="/about">About</Navlink>
+            </li>
+            <li>
+              <Navlink to="/contact">Contact</Navlink>
+            </li>
+          </Navmenu>
+          <Hambuger onClick={() => setExtendedNav(current => !current)}  />
+        </Navright>
+      </Navmain>
+      {extendedNav ? 
+      (<Navmenuextended>
         <li>
-          <Link to="/work">Work</Link>
+          <Navlink to="/work">Work</Navlink>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Navlink to="/about">About</Navlink>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Navlink to="/contact">Contact</Navlink>
         </li>
-      </Menu>
-    </S_Navbar>
+      </Navmenuextended>)
+      : ""}
+      
+    </StyledNavbar>
   );
 }
-const S_Navbar = styled.nav`
+const StyledNavbar = styled.nav`
   display: flex;
   flex-direction: column;
   text-align: center;
   font-size: 0.95rem;
-  span {
-    font-size: 1.3rem;
-  }
+  position: relative;
+
   li {
     list-style: none;
   }
 `;
-const Left = styled.div`
+const Navmain = styled.div`
   display: flex;
-  width: 100%;
   justify-content: space-between;
-  align-items: flex-start;
-  border-bottom: 0.5px solid gray;
+  width: 100%;
   padding-bottom: 0.5em;
   margin-bottom: 0.4em;
-`;
-const Menu = styled.ul``;
-const S_NavMain = styled.div`
   
+
+`;
+const Navleft = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+`;
+const Navright = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 40%;
+`;
+const Navmenu = styled.ul`
+display: none;
+  @media (min-width: 1025px) {
+    display: flex;
+    gap: 1.5em;
+
+
+  }
 `
-// const S_NavExtended
+const Navdownload = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 12px;
+  transform: translate(-50%, -50%);
+  &:hover {
+    color: blue;
+  }
+`
+
+const Navmenuextended = styled.ul`
+  font-size: 0.90rem;
+  @media (min-width: 1025px) {
+    display: none;
+  }
+`;
+const Navlink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  &:hover {
+    color: blue;
+  }
+`
+const Hambuger = styled(GiHamburgerMenu)`
+  @media (min-width: 1025px) {
+    display: none;
+  }
+`
+
